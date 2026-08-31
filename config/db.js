c2d2dc1baf9e3f83fa297bab4e4c3288aa69e4b5
Ayush Jaryal <tonistarktonistark61@gmail.com>
2026-08-31 23:39:37 +0530

    f

--- config/db.js
+++ config/db.js
@@ -1,16 +1,19 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  try {
-    await mongoose.connect(
-      "mongodb+srv://tonistarktonistark61_db_user:UIHcX6SZlmv5cSik@attendease.qahldzq.mongodb.net/AttendEase?retryWrites=true&w=majority&appName=attendease"
-    );
+    try {
 
-    console.log("MongoDB Atlas Connected");
-  } catch (err) {
-    console.error(err);
-    process.exit(1);
-  }
+        await mongoose.connect(process.env.MONGODB_URI);
+
+        console.log("MongoDB Atlas Connected");
+
+    } catch (err) {
+
+        console.error("MongoDB connection error:", err);
+
+        process.exit(1);
+
+    }
 };
 
 module.exports = connectDB;
